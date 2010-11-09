26c37c2e91e8f976d652cfda357dea86bda95248
Daniel Pittman <daniel@rimspace.net>
2010-11-10 09:51:46 +1100

    Increase the width of the package name in the formatted output.
    This makes the code a tiny bit cleaner when we have very long distribution
    names present like, oh, Dist::Zilla::Plugin::ReportVersions::Tiny, say.

--- lib/Dist/Zilla/Plugin/ReportVersions/Tiny.pm
+++ lib/Dist/Zilla/Plugin/ReportVersions/Tiny.pm
@@ -58,7 +58,7 @@ sub pmver {
     }
 
     # So, we should be good, right?
-    return sprintf('%-40s => %-10s%-15s%s', $module, $pmver, $wanted, "\n");
+    return sprintf('%-45s => %-10s%-15s%s', $module, $pmver, $wanted, "\n");
 }
 
 {{
