19f0a919634eff8532593028a5c7ec89996a3c99
Kent Fredric <kentfredric@gmail.com>
2012-03-18 13:12:33 +1300

    Fix the rest of the bug by fixing the spelling mistake

--- lib/Dist/Zilla/Plugin/ReportVersions/Tiny.pm
+++ lib/Dist/Zilla/Plugin/ReportVersions/Tiny.pm
@@ -69,7 +69,7 @@ $v .= <<'EOT';
 
 Thanks for using my code.  I hope it works for you.
 If not, please try and include this output in the bug report.
-That will help me reproduce the issue and solve you problem.
+That will help me reproduce the issue and solve your problem.
 
 EOT
 
